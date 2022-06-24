7bda43548c50ddd5e92ccba34fccea392c13a79e
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-06-24 10:57:52 -0300

    prototipo rotas

--- src/database.js
+++ src/database.js
@@ -18,14 +18,14 @@ export async function getAluguel(req, res) {
         .catch(err => console.error(err.sqlMessage));
 }
 
-export async function addAluguel(req, res) {
+export async function iniciarAluguel(req, res) {
     let qryText = `insert into aluguel(valorbase, cpfvendedor, cpfcliente, placacarro) values(${req.body.valorbase}, ${req.body.cpfvendedor}, ${req.body.cpfcliente}, ${req.body.placacarro});`;
     await connection.query(qryText)
         .then(result => res.send(result))
         .catch(err => console.error(err));
 }
 
-export async function endAluguel(req, res) {
+export async function encerrarAluguel(req, res) {
     let qryText = `update aluguel set ativo = 'N' where cpfcliente = ${req.body.cpfcliente} AND idaluguel = ${req.params.id};`;
     await connection.query(qryText)
         .then(result => res.send(result))
--- src/server.js
+++ src/server.js
@@ -7,6 +7,86 @@ import * as db from './database.js';
     const port = 5000;
     await db.connect();
 
+    // ROTAS CLIENTE
+    server.post('/cliente/cadastro', (req, res) => {
+        db.addCliente(req, res);
+    });
+
+    server.put('/cliente/atualizar', (req, res) => {
+        db.updCliente(req, res);
+    });
+
+    server.delete('/cliente/remover', (req, res) => {
+        db.rmvCliente(req, res);
+    });
+
+    server.get('/cliente/consultar', (req, res) => {
+        db.getCliente(req, res);
+    });
+
+    // ROTAS VENDEDOR
+    server.post('/vendedor/cadastro', (req, res) => {
+        db.addVendedor(req, res);
+    });
+
+    server.put('/vendedor/gerar-pagamentos', (req, res) => {
+        db.gerarPagementos(req, res);
+    });
+    server.put('/vendedor/atualizar', (req, res) => {
+        db.updVendedor(req, res);
+    });
+
+    server.delete('/vendedor/remover', (req, res) => {
+        db.rmvVendedor(req, res);
+    });
+
+    server.get('/vendedor/consultar', (req, res) => {
+        db.getVendedor(req, res);
+    });
+
+    // ROTAS CARRO
+    server.post('/frota/cadastro', (req, res) => {
+        db.addCarro(req, res);
+    });
+
+    server.put('/frota/atualizar', (req, res) => {
+        db.updCarro(req, res);
+    });
+
+    server.delete('/frota/remover', (req, res) => {
+        db.rmvCarro(req, res);
+    });
+
+    server.get('/frota/consultar', (req, res) => {
+        db.getCarro(req, res);
+    });
+
+    // ROTAS LOJA
+    server.post('/loja/cadastro', (req, res) => {
+        db.addLoja(req, res);
+    });
+
+    server.put('/loja/atualizar', (req, res) => {
+        db.updLoja(req, res);
+    });
+
+    server.delete('/loja/remover', (req, res) => {
+        db.rmvLoja(req, res);
+    });
+
+    server.get('/loja/consultar', (req, res) => {
+        db.getLoja(req, res);
+    });
+
+    // ROTAS ALUGUEL
+    server.post('/alugueis/novo', (req, res) => {
+        db.iniciarAluguel(req, res);
+    });
+
+    server.put('/alugueis/:id/encerrar', (req, res) => {
+        db.encerrarAluguel(req, res);
+    });
+
     server.get('/alugueis', (req, res) => {
         db.getAluguel(req, res);
     });
@@ -14,16 +94,8 @@ import * as db from './database.js';
         db.getAluguel(req, res);
     });
 
-    server.post('/alugueis/novo', (req, res) => {
-        db.addAluguel(req, res);
-    });
-
-    server.put('/alugueis/:id/encerrar', (req, res) => {
-        db.endAluguel(req, res);
-    })
-
     await server.listen(port, () => {
         console.log(`Listening on port ${port}`);
-    })
+    });
 
 })();
