a2056f79a740014d4075ce2dcd3e89deb8e43784
Victor Le Roy Matos <victorlrmatos@gmail.com>
2022-06-28 18:22:06 -0300

    [+] adicao de CORS para conexao

--- bd1_tp-back-end/src/app.js
+++ bd1_tp-back-end/src/app.js
@@ -10,6 +10,12 @@ import { default as rtrGeneral } from './routes/rtrGeneral.js'
 export const app = async () => {
     const server = express();
     server.use(express.json());
+    server.use(function (req, res, next) { 
+        res.header("Access-Control-Allow-Origin", "*"); 
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token"); 
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); 
+        next(); 
+    });
     const port = 5000;
 
     server.use('/aluguel', rtrAluguel);
