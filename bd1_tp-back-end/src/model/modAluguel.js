828a48ab6ad80935e23f1d89bea286af4abd8d87
Arthur Severo <svzr.47@gmail.com>
2022-06-28 18:01:05 -0300

    getaluguel update

--- bd1_tp-back-end/src/model/modAluguel.js
+++ bd1_tp-back-end/src/model/modAluguel.js
@@ -3,7 +3,10 @@ const db = await connection;
 
 // QUERYS ALUGUEL
 export async function getAluguel(req, res) {
-    let qryText = `select * from aluguel where idaluguel = ${req.params.id};`;
+    let qryText = `select * from aluguel`;
+    if (req.params.id)
+        qryText = qryText + `where idaluguel = ${req.params.id};`;
+    qryText = qryText + ';';
     await db.query(qryText)
         .then(result => res.send(result.at(0)))
         .catch(err => console.error(err.sqlMessage));
