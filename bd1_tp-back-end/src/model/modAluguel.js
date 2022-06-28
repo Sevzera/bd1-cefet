335ac24c503efc21c8964e4612fbd7b5c4891b0d
Arthur Severo <svzr.47@gmail.com>
2022-06-28 18:05:55 -0300

    modaluguel fix v3

--- bd1_tp-back-end/src/model/modAluguel.js
+++ bd1_tp-back-end/src/model/modAluguel.js
@@ -5,7 +5,7 @@ const db = await connection;
 export async function getAluguel(req, res) {
     let qryText = `select * from aluguel`;
     if (req.params.id)
-        qryText = qryText + `where idaluguel = ${req.params.id}`;
+        qryText = qryText + ` where idaluguel = ${req.params.id}`;
     qryText = qryText + ';';
     await db.query(qryText)
         .then(result => res.send(result.at(0)))
