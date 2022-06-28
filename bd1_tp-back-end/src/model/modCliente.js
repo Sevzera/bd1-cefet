a699970961c12c686f2ee0c3ef2e51352e4b4344
Arthur Severo <svzr.47@gmail.com>
2022-06-28 18:08:13 -0300

    getallclientes

--- bd1_tp-back-end/src/model/modCliente.js
+++ bd1_tp-back-end/src/model/modCliente.js
@@ -35,7 +35,10 @@ export async function rmvCliente(req, res) {
 }
 
 export async function getCliente(req, res) {
-    let qryText = `select * from cliente where cpf = ${req.body.cpf};`
+    let qryText = `select * from cliente`;
+    if (req.params.id)
+        qryText = qryText + ` where cpf = ${req.body.cpf}`;
+    qryText = qryText + ';';
     await db.query(qryText)
         .then(result => res.send(result.at(0)))
         .catch(err => console.error(err.sqlMessage));
